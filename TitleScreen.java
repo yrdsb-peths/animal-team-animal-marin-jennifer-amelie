89f813c065acc0191aa0d33c516477229cabadfd
marin-yan <marinxyan@gmail.com>
2025-05-12 09:16:05 -0400

    added a background for the title page

--- TitleScreen.java
+++ TitleScreen.java
@@ -16,6 +16,9 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        GreenfootImage bg = new GreenfootImage("images/background.png");
+        getBackground().drawImage(bg, 0, 0);
 
         addObject(titleLabel, 250, 125);
         prepare();
