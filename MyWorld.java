195c7baeb289feacf82728256a661a6a926ace43
marin-yan <marinxyan@gmail.com>
2025-05-12 08:45:48 -0400

    added Apple to the world

--- MyWorld.java
+++ MyWorld.java
@@ -4,4 +4,13 @@ public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
     }
+    
+    /**
+     * Create a new apple at random location at top of screen
+     */
+    public void createApple() {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        addObject(apple, x, 0);
+    }
 }
